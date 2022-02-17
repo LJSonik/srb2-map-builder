86bff631d5433f3a59ff4a8f7d985dbb5cc956c1
LJ Sonic <lamr@free.fr>
2022-02-18 00:06:43 +0100

    Send key events to focused item

--- PK3/Lua/Libraries/ljgui/Keyboard.lua
+++ PK3/Lua/Libraries/ljgui/Keyboard.lua
@@ -4,8 +4,20 @@ local gui = ljrequire "ljgui.common"
 
 ---@type keyevent_t
 function gui.handleKeyDown(key)
+    local item = gui.root.focusedItem
+    if not item then return end
+
+    if item.onKeyDown then
+        item:onKeyDown(key)
+    end
 end
 
 ---@type keyevent_t
 function gui.handleKeyUp(key)
+    local item = gui.root.focusedItem
+    if not item then return end
+
+    if item.onKeyUp then
+        item:onKeyUp(key)
+    end
 end
