94b04294a19aa056133556a12dae72f1b94eaf56
LJ Sonic <lamr@free.fr>
2022-02-28 00:30:38 +0100

    Add item-bound key events

--- PK3/Lua/Libraries/ljgui/Keyboard.lua
+++ PK3/Lua/Libraries/ljgui/Keyboard.lua
@@ -4,11 +4,15 @@ local gui = ljrequire "ljgui.common"
 
 ---@type keyevent_t
 function gui.handleKeyDown(key)
+    local root = gui.root
+
+    if root:callEvent("KeyDown", key) then return end
+
     if key.name == "mouse1" then
-        gui.root.mouse:pressLeftButton()
+        root.mouse:pressLeftButton()
     end
 
-    local item = gui.root.focusedItem
+    local item = root.focusedItem
     if not item then return end
 
     if item.onKeyDown then
@@ -18,11 +22,15 @@ end
 
 ---@type keyevent_t
 function gui.handleKeyUp(key)
+    local root = gui.root
+
+    if root:callEvent("KeyUp", key) then return end
+
     if key.name == "mouse1" then
-        gui.root.mouse:releaseLeftButton()
+        root.mouse:releaseLeftButton()
     end
 
-    local item = gui.root.focusedItem
+    local item = root.focusedItem
     if not item then return end
 
     if item.onKeyUp then
